8f7148052b46e33b8a2c05eccefb8d03da8557f8
Igor Bondarenko <bondarenko200021@mail.ru>
2025-09-27 17:20:34 +0700

    add uni-tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,22 @@
 import pytest_asyncio
+import asyncio
+import pytest
+from application.schemas.order import CreateOrder
 from application.schemas.user import CreateUser
 from application.db.database import test_db_helper
 from application.crud.user import create_user_crud
+from application.crud.order import create_order_crud
+
+
+@pytest_asyncio.fixture(scope="session")
+async def event_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
 
 # фикстура создания пользователя(сам объект)
-@pytest_asyncio.fixture
+@pytest_asyncio.fixture(scope="function")
 async def make_user_data():
     user = CreateUser(
         email="john@example.com",
@@ -16,7 +27,16 @@ async def make_user_data():
     return user
 
 
-@pytest_asyncio.fixture
+@pytest_asyncio.fixture(scope="function")
+async def make_order_data(create_user_db):
+    order = CreateOrder(
+        status="packed",
+        user_id=create_user_db.id,
+    )
+    return order
+
+
+@pytest_asyncio.fixture(scope="function")
 async def make_user_data_2():
     user = CreateUser(
         email="john@example.com",
@@ -29,7 +49,7 @@ async def make_user_data_2():
 
 # функция создания сессии для подключение к БД
 @pytest_asyncio.fixture
-async def session_test_db():
+async def session_test_db(scope="function"):
     async for session in test_db_helper.get_session():
         try:
             yield session
@@ -39,10 +59,19 @@ async def session_test_db():
 
 
 # crud функция которая создаст строку в тестовой БД
-@pytest_asyncio.fixture
+@pytest_asyncio.fixture(scope="function")
 async def create_user_db(make_user_data, session_test_db):
     new_user = await create_user_crud(
         user_data=make_user_data,
         session=session_test_db,
     )
     return new_user
+
+
+@pytest_asyncio.fixture(scope="function")
+async def create_order_db(make_order_data, session_test_db):
+    new_order = await create_order_crud(
+        order_data=make_order_data,
+        session=session_test_db,
+    )
+    return new_order
--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import pytest
+from application.models.user import UserOrm
 from application.utils import verify_password
 from sqlalchemy.exc import IntegrityError
-from application.crud.user import create_user_crud
+from application.crud.user import create_user_crud, get_user_by_id_crud
 
 
 @pytest.mark.asyncio
@@ -23,3 +24,25 @@ async def test_create_user(
     # попытка создать 2 пользователя, чтобы проверить исключение
     with pytest.raises(IntegrityError):
         await create_user_crud(user_data=make_user_data_2, session=session_test_db)
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_id(
+    create_user_db,
+    session_test_db,
+    make_user_data,
+    make_order_data,
+    create_order_db,
+):
+    response = await get_user_by_id_crud(
+        user_id=create_user_db.id,
+        session=session_test_db,
+    )
+    assert response is not None
+    assert isinstance(create_user_db.id, int)
+    assert isinstance(response, UserOrm)
+    assert response.username == make_user_data.username
+    assert response.email == make_user_data.email
+
+    # assert response.orders[0].status == make_order_data.status
+    # assert response.orders[0].user_id == make_order_data.user_id
